e406e1cb2df11ce9ad5094d31afe53f809dfc263
Svitlana Marchenko <sveta25022004@gmail.com>
2023-08-07 22:00:04 +0300

    add auto generated id

--- src/main/org/marchenkosv/dao/PersonDAO.java
+++ src/main/org/marchenkosv/dao/PersonDAO.java
@@ -32,7 +32,7 @@ public class PersonDAO {
     }
 
     public void save(Person person) {
-        jdbcTemplate.update("INSERT INTO Person VALUES(?, ?, ?, ?)", PEOPLE_COUNT++, person.getName(), person.getAge(),
+        jdbcTemplate.update("INSERT INTO Person (name, age, email) VALUES(?, ?, ?)", person.getName(), person.getAge(),
                 person.getEmail());
     }
 
